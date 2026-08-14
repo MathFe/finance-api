e510d50d266ec652e12456bfd579f745ebed5eea
mathfe <matheusferreirasjc@gmail.com>
2026-08-13 23:42:02 -0300

    feat: added the budget entity

--- src/main/java/com/mathfe/finance/Entity/Budget.java
+++ src/main/java/com/mathfe/finance/Entity/Budget.java
@@ -1,4 +1,44 @@
 package com.mathfe.finance.Entity;
 
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tb_budget")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
 public class Budget {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer month;
+
+    @Column(nullable = false)
+    private Integer year;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+
 }
