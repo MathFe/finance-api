a0606719b4530209ff31002555f100c35c7c5557
mathfe <matheusferreirasjc@gmail.com>
2026-08-14 19:46:56 -0300

    feat(entity): add BudgetItem entity

--- src/main/java/com/mathfe/finance/Entity/BudgetItem.java
+++ src/main/java/com/mathfe/finance/Entity/BudgetItem.java
@@ -1,4 +1,32 @@
 package com.mathfe.finance.Entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "tb_itensbudget")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
 public class BudgetItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "budget_id", nullable = false)
+    private Budget budget;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+
+    @Column(name = "planned_amount", nullable = false, precision = 12, scale = 2)
+    private BigDecimal plannedAmount;
+
 }
