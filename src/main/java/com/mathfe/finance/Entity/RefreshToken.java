30a53597ce285f4f9f1b883a96dc29c08e366b8e
mathfe <matheusferreirasjc@gmail.com>
2026-08-14 23:12:08 -0300

    feat(entity): add RefreshToken entity

--- src/main/java/com/mathfe/finance/Entity/RefreshToken.java
+++ src/main/java/com/mathfe/finance/Entity/RefreshToken.java
@@ -1,4 +1,39 @@
 package com.mathfe.finance.Entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tb_refreshtoken")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
 public class RefreshToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false, unique = true, length = 255)
+    private String token;
+
+    @Column(name = "expiry_date", nullable = false)
+    private LocalDateTime expiryDate;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+
 }
