193ba41ed0433c325d37b28e4ce9876b398daa01
mathfe <matheusferreirasjc@gmail.com>
2026-08-17 23:36:35 -0300

    feat(repository): add UserRepository and CategoryRepository

--- src/main/java/com/mathfe/finance/Repository/CategoryRepository.java
+++ src/main/java/com/mathfe/finance/Repository/CategoryRepository.java
@@ -1,4 +1,15 @@
 package com.mathfe.finance.Repository;
 
-public interface CategoryRepository {
+import com.mathfe.finance.Entity.Category;
+import com.mathfe.finance.Entity.CategoryType;
+import com.mathfe.finance.Entity.User;
+import org.aspectj.weaver.ast.And;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    List<Category> findByUser(User user);
+
+    List<Category> findByUserAndType( User user, CategoryType categoryType);
 }
--- src/main/java/com/mathfe/finance/Repository/UserRepository.java
+++ src/main/java/com/mathfe/finance/Repository/UserRepository.java
@@ -1,4 +1,13 @@
 package com.mathfe.finance.Repository;
 
-public interface UserRepository {
+import com.mathfe.finance.Entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository  extends JpaRepository<User,Long> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
 }
