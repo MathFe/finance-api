92a3cc74a64acff9178888c321378c0b34397dc9
mathfe <matheusferreirasjc@gmail.com>
2026-08-21 23:43:37 -0300

    feat(dto): add CategoryRequestDTO and CategoryResponseDTO

--- src/main/java/com/mathfe/finance/dto/CategoryRequestDTO.java
+++ src/main/java/com/mathfe/finance/dto/CategoryRequestDTO.java
@@ -1,4 +1,18 @@
 package com.mathfe.finance.dto;
 
-public record CategoryRequestDTO() {
+import com.mathfe.finance.entity.CategoryType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record CategoryRequestDTO(
+
+        @NotBlank(message = "Name is required")
+        String name,
+
+        @NotNull(message = "Type is required")
+        CategoryType type,
+
+        @NotBlank(message = "Color is required")
+        String color
+) {
 }
--- src/main/java/com/mathfe/finance/dto/CategoryResponseDTO.java
+++ src/main/java/com/mathfe/finance/dto/CategoryResponseDTO.java
@@ -1,4 +1,15 @@
 package com.mathfe.finance.dto;
 
-public record CategoryResponseDTO() {
+import com.mathfe.finance.entity.CategoryType;
+
+import java.time.LocalDateTime;
+
+public record CategoryResponseDTO(
+
+        Long id,
+        String name,
+        CategoryType type,
+        String color,
+        LocalDateTime createdAt
+) {
 }
