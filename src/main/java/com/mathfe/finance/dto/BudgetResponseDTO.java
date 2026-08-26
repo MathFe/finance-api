4b79eb3c55270a222cd325aba33b44173ef078c9
mathfe <matheusferreirasjc@gmail.com>
2026-08-25 23:44:34 -0300

    feat(service): add UserService with password hashing and PasswordEncoder bean

--- src/main/java/com/mathfe/finance/dto/BudgetResponseDTO.java
+++ src/main/java/com/mathfe/finance/dto/BudgetResponseDTO.java
@@ -4,13 +4,11 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public record BudgetResponseDTO(
-
         Long id,
         String name,
         Integer month,
         Integer year,
         LocalDateTime createdAt,
         List<BudgetItemResponseDTO> items
-
 ) {
 }
--- src/main/java/com/mathfe/finance/service/UserService.java
+++ src/main/java/com/mathfe/finance/service/UserService.java
@@ -1,4 +1,42 @@
 package com.mathfe.finance.service;
 
+import com.mathfe.finance.dto.UserRequestDTO;
+import com.mathfe.finance.dto.UserResponseDTO;
+import com.mathfe.finance.entity.User;
+import com.mathfe.finance.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public UserResponseDTO register(UserRequestDTO dto) {
+
+        if (userRepository.existsByEmail(dto.email())) {
+            throw new IllegalArgumentException("Email already in use");
+        }
+
+        User user = User.builder()               //Change to mapper later
+                .name(dto.name())
+                .email(dto.email())
+                .password(passwordEncoder.encode(dto.password()))
+                .build();
+
+        User savedUser = userRepository.save(user);
+
+        return new UserResponseDTO(
+                savedUser.getId(),
+                savedUser.getName(),
+                savedUser.getEmail()
+        );
+    }
 }
